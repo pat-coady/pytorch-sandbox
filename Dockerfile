3e11a2e30f6dccfd9dcb519abdc21dbd55dacaa3
Patrick Coady <patrick.coady@analog.com>
2024-01-17 23:18:18 +0000

    Change base container image.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 # no GPU
-# FROM mcr.microsoft.com/vscode/devcontainers/base:0-ubuntu-20.04
+FROM mcr.microsoft.com/vscode/devcontainers/base:0-ubuntu-20.04
 
 # with GPU
-FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+# FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
 
 RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
     && apt-get -y install --no-install-recommends python3.10 python3-pip
--- scripts/nvidia_k80.sh
+++ scripts/nvidia_k80.sh
@@ -1,7 +1,10 @@
 # install NVIDIA driver for EC2 p2.xlarge instance
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo apt-get install -y pkg-config
 sudo apt-get install xorg
-sudo apt-get install nvidia-driver-460
 
-sudo reboot
+BASE_URL=https://us.download.nvidia.com/tesla
+DRIVER_VERSION=470.223.02
+curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
+sudo sh NVIDIA-Linux-x86_64-470.223.02.run
